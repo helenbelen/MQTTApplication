20b35786b16956a9c4c3c7ba4a13af13152d6ee8
HelenBelen <helenbelen111@gmail.com>
2018-02-23 22:02:47 -0500

    Revert "Revert "Create New API To Access Device Data and Update Device Info""
    This reverts commit 34b5add0253467f8cbedc8445ebe090821854bd4.

---
+++ HomeAutomationAPI/Models/ConnectionInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeAutomationAPI.Models
+{
+    public partial class ConnectionInfo
+    {
+        public int InfoId { get; set; }
+        public string InfoName { get; set; }
+        public string InfoString { get; set; }
+    }
+}
