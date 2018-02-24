2474c65faf64c83edb50fbbca8d4c87de1266c79
HelenBelen <helenbelen111@gmail.com>
2018-02-23 21:52:49 -0500

    Create New API To Access Device Data and Update Device Info

---
+++ HomeAutomationAPI/Models/DeviceList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeAutomationAPI.Models
+{
+    public partial class DeviceList
+    {
+        public int DeviceId { get; set; }
+        public string DeviceName { get; set; }
+        public string DeviceLocation { get; set; }
+
+        public DeviceList Device { get; set; }
+        public DeviceList InverseDevice { get; set; }
+    }
+}
