9069b9c9ab0162d09b4babdfb4b28406feab53f1
HelenBelen <helenbelen111@gmail.com>
2018-02-24 13:03:37 -0500

    Create New Controller To Handle Items On The DeviceList
    DeviceList Controller methods work in local environment but don't work on server.

---
+++ WebAPI/Controllers/DeviceDataController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class DeviceDataController : Controller
+    {
+        private readonly HomeAutomationContext _context;
+        public DeviceDataController(HomeAutomationContext context)
+        {
+            _context = context;
+        }
+        // GET: api/DeviceData
+       
+             
+        [HttpGet]
+        public IEnumerable<DeviceData> Get()
+        {
+            return _context.DeviceData.ToList();
+        }
+
+        //GET: api/DeviceData/GetConnectionInfo
+        [Route("~/api/DeviceData/GetConnectionInfo")]
+        [HttpGet]
+        public IActionResult GetConnectionInfo()
+        {
+            var ip = _context.ConnectionInfo.FirstOrDefault(d => d.InfoName == "mosquitto");
+            if(ip == null)
+            {
+                return new NoContentResult();
+            }
+            return new ObjectResult(ip.InfoString);
+        }
+
+       
+        //GET: api/DeviceData/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            
+            var deviceData = _context.DeviceData.Where(d => d.DeviceId == id);
+          
+            return new ObjectResult(deviceData);
+        }
+
+
+        // POST: api/DeviceData/5
+
+        [HttpPost("{id}")]
+        public IActionResult Post(int id, [FromBody]string data)
+        {
+             if(_context.DeviceList.Any(d => d.DeviceId == id && d.DeviceName != null))
+            {
+                DeviceData deviceData = new DeviceData();
+                deviceData.DeviceId = id;
+                deviceData.Timestamp = DateTime.Now;
+                deviceData.Data = data;
+                _context.DeviceData.Add(deviceData);
+                _context.SaveChanges();
+                return new ObjectResult("Data Has Been Added!");
+            }
+            return BadRequest("Data has not been added. Please ensure Device has been registered");
+            
+        }
+
+
+        
+    }
+}
---
+++ WebAPI/Controllers/DeviceListController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class DeviceListController : Controller
+    {
+        private readonly HomeAutomationContext _context;
+        public DeviceListController(HomeAutomationContext context)
+        {
+            _context = context;
+        }
+        // GET: api/DeviceList
+       
+        [HttpGet]
+        public IEnumerable<DeviceList> Get()
+        {
+
+            return _context.DeviceList.ToList();
+        }
+
+        // GET: api/DeviceList/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+
+            var deviceData = _context.DeviceList.Where(d => d.DeviceId == id);
+
+            return new ObjectResult(deviceData);
+        }
+
+                     
+
+        // POST: api/DeviceList
+        [HttpPost]
+        public IActionResult Post([FromBody]string name)
+        {
+            if (_context.DeviceList.Any(d => d.DeviceName == name))
+            {
+                return new ObjectResult("This Device Has Already Been Registered");
+            }
+            else
+            {
+                var device = _context.DeviceList.First(d => d.DeviceName == null);
+                if (device != null)
+                {
+                    device.DeviceName = name;
+                    _context.Update(device);
+                    _context.SaveChanges();
+                    return new ObjectResult("Your Device is registered and has a device Id of: " + device.DeviceId + " Please keep this for your records. You will not be able to retrieve this again without contacting the API Admin");
+                }
+            }
+            return BadRequest("An error has occured and your device was not registered. Please contact the API Admin");
+        }
+        
+        // PUT: api/DeviceList/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]string location)
+        {
+            if (_context.DeviceList.Any(d => d.DeviceId == id && d.DeviceName != null))
+            {
+                var device = _context.DeviceList.First(d => d.DeviceId == id);
+                device.DeviceLocation = location;
+                _context.Update(device);
+                _context.SaveChanges();
+                return new ObjectResult("Location has been updated for Device #: " + id);
+            }
+            return BadRequest("Location has not been updated. Please ensure Device has been registered");
+        }
+        
+       
+    }
+}
