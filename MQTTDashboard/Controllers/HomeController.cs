5d7a6bdea777bd21bda5664a9e3382dc9041761b
HelenBelen <helenbelen111@gmail.com>
2018-02-24 02:29:07 -0500

    Create User Interface that displays data in database
    There is an issue with showing the list of devices, the device by ID, and saving data by ID with a device that has been registered.

---
+++ MQTTDashboard/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MQTTDashboard.Models;
+using System.Configuration;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MQTTDashboard.Controllers
+{
+    public class HomeController : Controller
+    {
+        static private readonly string baseURL = "http://webapi-dev.us-east-1.elasticbeanstalk.com/api/Device/";
+        static HttpClient httpClient;
+        static List<DataItem> dataItems = new List<DataItem>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public  IActionResult DataItems()
+        {
+            dataItems.Clear();
+            GetDeviceData();
+            return View(dataItems);
+          
+        }
+
+
+        public static async Task GetDeviceData()
+        {
+            httpClient = new HttpClient();
+            
+            using (var httpClient = new HttpClient())
+            {
+
+                var response =  httpClient.GetStringAsync(new Uri(baseURL + "GetData")).Result;
+
+                var releases = JArray.Parse(response);
+                foreach (Object o in releases)
+                {
+                    dataItems.Add(JsonConvert.DeserializeObject<DataItem>(o.ToString()));
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ MQTTDashboard/Models/DataItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace MQTTDashboard.Models
+{
+    [Serializable]
+    public class DataItem
+    {
+        public int DeviceId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Data { get; set; }
+    }
+}
