431cd5f20025d3ceea0bc1f1451fe7aebf1a9cf2
HelenBelen <helenbelen111@gmail.com>
2018-02-24 00:19:17 -0500

    Re-did API For Data Access

---
+++ WebAPI/Controllers/DeviceController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class DeviceController : Controller
+    {
+        private readonly HomeAutomationContext _context;
+        public DeviceController(HomeAutomationContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Device
+       
+        [HttpGet(Name ="Get")]
+        public IEnumerable<DeviceList> GetDevices()
+        {
+           
+            return _context.DeviceList.ToList();
+        }
+        [Route("~/api/Device/GetData")]
+
+        [HttpGet]
+        public IEnumerable<DeviceData> GetData()
+        {
+            return _context.DeviceData.ToList();
+        }
+
+        [Route("~/api/Device/GetConnectionInfo")]
+
+        [HttpGet]
+        public IActionResult GetConnectionInfo()
+        {
+            var ip = _context.ConnectionInfo.FirstOrDefault(d => d.InfoName == "mosquitto");
+            if(ip == null)
+            {
+                return new NoContentResult();
+            }
+            return new ObjectResult(ip.InfoString);
+        }
+
+ 
+        [Route("~/api/Device/GetDataByID/{id}")]
+        [HttpGet]
+        //HttpGet("{id}",Name ="GetDataById")]
+        public IActionResult Get(int id)
+        {
+            
+            var deviceData = _context.DeviceData.Where(d => d.DeviceId == id);
+          
+            return new ObjectResult(deviceData);
+        }
+
+        
+       [ HttpGet("GetDeviceByID/{id}",Name ="GetDataById")]
+        public IActionResult GetDevice(int id)
+        {
+
+            var deviceData = _context.DeviceList.Where(d => d.DeviceId == id);
+
+            return new ObjectResult(deviceData);
+        }
+
+        // POST: api/Device
+        [Route("~/api/Device/AddData/{id}/{data}")]
+        [HttpPost]
+        public IActionResult Post(int id, string data)
+        {
+             if(_context.DeviceList.Any(d => d.DeviceId == id && d.DeviceName != null))
+            {
+                DeviceData deviceData = new DeviceData();
+                deviceData.DeviceId = id;
+                deviceData.Timestamp = DateTime.Now;
+                deviceData.Data = data;
+                _context.DeviceData.Add(deviceData);
+                _context.SaveChanges();
+                return new ObjectResult("Data Has Been Added!");
+            }
+            return BadRequest("Data has not been added. Please ensure Device has been registered");
+            
+        }
+
+        // PUT: api/Device/5
+        [Route("~/api/Device/UpdateDeviceLocation/{id}/{location}")]
+        [HttpPut]
+        public IActionResult Put(int id, string location)
+        {
+            if (_context.DeviceList.Any(d => d.DeviceId == id && d.DeviceName != null))
+            {
+               var device = _context.DeviceList.First(d => d.DeviceId == id);
+                device.DeviceLocation = location;
+                _context.Update(device);
+                _context.SaveChanges();
+                return new ObjectResult("Location has been updated for Device #: " + id);
+            }
+            return BadRequest("Location has not been updated. Please ensure Device has been registered");
+        }
+
+        [Route("~/api/Device/RegisterDevice/{name}")]
+        [HttpPut]
+        public IActionResult RegisterDevice(string name)
+        {
+            if (_context.DeviceList.Any(d => d.DeviceName == name)) {
+                return new ObjectResult("This Device Has Already Been Registered");
+            }
+            else {
+                var device = _context.DeviceList.First(d => d.DeviceName == null);
+                if (device != null)
+                {
+                    device.DeviceName = name;
+                    _context.Update(device);
+                    _context.SaveChanges();
+                    return new ObjectResult("Your Device is registered and has a device Id of: " + device.DeviceId + " Please keep this for your records. You will not be able to retrieve this again without contacting the API Admin");
+                }
+            }
+            return BadRequest("An error has occured and your device was not registered. Please contact the API Admin");
+        }
+
+        
+    }
+}
---
+++ WebAPI/Models/HomeAutomationContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebAPI.Models
+{
+    public partial class HomeAutomationContext : DbContext
+    {
+        public virtual DbSet<ConnectionInfo> ConnectionInfo { get; set; }
+        public virtual DbSet<DeviceData> DeviceData { get; set; }
+        public virtual DbSet<DeviceList> DeviceList { get; set; }
+
+        public HomeAutomationContext(DbContextOptions<HomeAutomationContext> options): base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"Server=mosquittodatabase.cffo0eijbrmb.us-east-1.rds.amazonaws.com,1433;Initial Catalog=MQTT;Integrated Security=False;User ID=admin;Password=mosquitto;Connect Timeout=30;TrustServerCertificate=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ConnectionInfo>(entity =>
+            {
+                entity.HasKey(e => e.InfoId);
+
+                entity.Property(e => e.InfoId)
+                    .HasColumnName("InfoID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.InfoName)
+                    .IsRequired()
+                    .HasColumnType("nchar(10)");
+
+                entity.Property(e => e.InfoString)
+                    .IsRequired()
+                    .HasColumnType("text");
+            });
+
+            modelBuilder.Entity<DeviceData>(entity =>
+            {
+                entity.HasKey(e => e.DeviceId);
+
+                entity.Property(e => e.DeviceId).ValueGeneratedNever();
+
+                entity.Property(e => e.Data)
+                    .IsRequired()
+                    .HasColumnType("nchar(10)");
+
+                entity.Property(e => e.Timestamp).HasColumnType("smalldatetime");
+            });
+
+            modelBuilder.Entity<DeviceList>(entity =>
+            {
+                entity.HasKey(e => e.DeviceId);
+
+                entity.Property(e => e.DeviceId)
+                    .HasColumnName("DeviceID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.DeviceLocation).HasColumnType("nchar(10)");
+
+                entity.Property(e => e.DeviceName).HasColumnType("nchar(10)");
+
+                entity.HasOne(d => d.Device)
+                    .WithOne(p => p.InverseDevice)
+                    .HasForeignKey<DeviceList>(d => d.DeviceId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_DeviceList_DeviceList");
+            });
+        }
+    }
+}
